6c30af381b6c3c6594f5aed0125eb043ac40cb70
Tomoyuki Mano <tomoyukimano@gmail.com>
2023-07-25 15:52:01 +0900

    update

--- psychopy/main1.py
+++ psychopy/main1.py
@@ -1,5 +1,4 @@
-from psychopy import visual, core, data, logging
-import numpy as np
+from psychopy import visual, data, logging
 import os
 from vstim import drifting_gratings, DriftingGratingsParams
 
@@ -21,7 +20,7 @@ if __name__ == "__main__":
     win = visual.Window(monitor='projector', size=[2560,1440], 
                         fullscr=True, screen=1,
                         units='pix', color=[0,0,0], allowGUI=False, waitBlanking=True)
-    
+
     p = DriftingGratingsParams(
         SFs=[0.01,0.03,0.05,0.07,0.1],
         TFs=[1,3,5,7,12],
---
+++ psychopy/vstim/__init__.py
@@ -0,0 +1,1 @@
+from .drifting_gratins import *
