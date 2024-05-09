f1f647678ea84401dc7a65c09c8374bc494e9865
Tomoyuki Mano <tomoyukimano@gmail.com>
2024-05-09 16:32:56 +0900

    update

---
+++ psychopy/tomo/main_temporal_chirp.py
@@ -0,0 +1,70 @@
+from psychopy import visual, data, event, logging
+import os, time
+from datetime import datetime
+from serial import Serial
+
+import sys
+sys.path.append("..")
+from vstim import temporal_chirp, TemporalChirpParams
+
+if __name__ == "__main__":
+    """
+    """
+
+    ###### PARAMETERS BEGIN ######
+    exp_name = "squid1_rec7"
+    logdir = r"D:\experiments\20240508"
+    p = TemporalChirpParams(
+        f0=0.5,
+        f1=10,
+        repeats=50,
+        trial_time=8,
+        interval_time=5
+    )
+    com_port = "COM3" # for DLP-IO8-G
+    ###### PARAMETERS END ######
+
+    # initialize DLP-IO8-G
+    dlp = Serial(port=com_port, baudrate=115200)
+
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d_%H%M%S")
+    log_filename_raw = os.path.join(logdir, f"log_{exp_name}_{dt_string}_raw.log")
+    log_filename =  os.path.join(logdir, f"log_{exp_name}_{dt_string}.csv")
+    # this is to log all events
+    log_file = logging.LogFile(log_filename_raw, level=logging.EXP)
+    # this is to log important events
+    exp_handler = data.ExperimentHandler(name=exp_name, version='',
+                                        extraInfo={},
+                                        runtimeInfo=None,
+                                        dataFileName=log_filename,
+                                        saveWideText=True,
+                                        savePickle=False)
+
+    win = visual.Window(monitor='DLP3010EVM-LC', size=[1280,720],
+                        fullscr=True, screen=1,
+                        units='pix', color=[0,0,0], allowGUI=False, waitBlanking=True)
+
+    # wait for TTL HIGH in channel 2 or keyboard input
+    while True:
+        dlp.write(b'S')  # request to read
+        x = dlp.read(3).decode('utf-8')
+        if x[0] == '1':
+            break # the line is HIGH
+        keys = event.getKeys()
+        if keys:
+            break
+
+    time.sleep(5.0) # wait 5 sec before proceeding
+    # start session; generate TTL pulses from channel 1
+    temporal_chirp(win, exp_handler, p, dlp=dlp, code_on=b'1', code_off=b'Q')
+    time.sleep(3.0) # wait 3 sec before proceeding
+
+    exp_handler.close()
+    win.close()
+
+    # using channel 3, send TTL to DAQ to notify the completion of the session
+    dlp.write(b'3')
+    time.sleep(0.5)
+    dlp.write(b'E')
+    dlp.close()
---
+++ psychopy/vstim/temporal_chirp.py
@@ -0,0 +1,68 @@
+from psychopy import visual, core, event, logging, clock
+import numpy as np
+from scipy import signal
+import os
+from dataclasses import dataclass, field
+
+@dataclass
+class TemporalChirpParams:
+    f0: float = 0.5 # Frequency in Hz at time t=0.
+    f1: float = 10 # Frequency in Hz at time t = end
+    repeats: int = 10 # number of repeats
+    trial_time: float = 20 # The total length of the chirp stimulation
+    interval_time: float = 2 # Interval between repeats in seconds
+
+def temporal_chirp(win, exp_handler, p: TemporalChirpParams, dlp=None, code_on=b'1', code_off=b'Q', save_movie=False):
+    """
+    This function generates drifting gratings pattern on a screen.
+
+    Parameters
+    ----------
+    win: psychopy.visual.Window object
+        Window must be set up by the parent python code and passed to this function.
+    exp_handler: psychopy.data.ExperimentHandler
+        ExperimentHandler object must be set up by the parent python code and passed to this function.
+    p:
+        Parameters for temporal chirp
+    dlp: serial.Serial object
+        This is to generate TTL pulses via DLP-IO8-G. If you don't need this, make this value None
+    code_on: str
+        Byte code to switch to HIGH
+    code_off: str
+        Byte code to switch to LOW
+    """
+
+    framerate = win.getActualFrameRate()
+    trial_frames = int(p.trial_time * framerate) # conver to secs to frames
+    interval_frames = int(p.trial_time * framerate) # conver to secs to frames
+
+    t = np.linspace(0, p.trial_time, int(p.trial_time*framerate))
+    w = signal.chirp(t, f0=p.f0, f1=p.f1, t1=p.trial_time, method='linear')
+
+    ###### Initiate Stimulus ########
+    frame_counter = 0
+    stop_loop=False
+
+    for rep in range(p.repeats):
+        # show inverval frames, i.e. blank image
+        for i in range(interval_frames):
+            frame_counter += 1
+            if dlp is not None:
+                dlp.write(code_off)
+            win.color = [0, 0, 0]
+            win.flip()
+
+        # temporal chirp starts
+        if dlp is not None:
+            dlp.write(code_on)
+        for v in w:
+            win.color = [v, v, v]
+            win.flip()
+
+        keys = event.getKeys()
+        if any(k in ['q','escape'] for k in keys):
+            stop_loop=True
+        event.clearEvents()
+        exp_handler.nextEntry()
+        if stop_loop==True:
+            break
